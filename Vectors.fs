71e96956b4d99a2bb62d666e6b13b249ab4d26a3
kleethesama <38240805+kleethesama@users.noreply.github.com>
2022-07-30 15:16:05 +0200

    New functions and types

--- Vectors.fs
+++ Vectors.fs
@@ -1,7 +1,13 @@
 module vectors
 
+open System
+
 type vector2D<[<Measure>]'u> = {x : float<'u>; y : float<'u>}
 type vector3D<[<Measure>]'u> = {x : float<'u>; y : float<'u>; z : float<'u>}
+[<Measure>] type degrees
+
+let degreesToRad (θ : float<degrees>) =
+  Math.PI * θ / 180.0 / 1.0<degrees>
 
 let newVector2D (x1 : float<'u>) (y1 : float<'u>) =
   let object : vector2D<'u> = {x = x1; y = y1}
@@ -15,6 +21,13 @@ let subtractVectors2D (v1 : vector2D<'u>) (v2 : vector2D<'u>) =
 
 let magnitudeOfVector2D (v1 : vector2D<'u>) =
   v1.x * v1.x + v1.y * v1.y |> sqrt
+
+let rotateVector2D (v1 : vector2D<'u>) (θ : float) =
+  let D = v1 |> magnitudeOfVector2D
+  (D * Math.Cos(θ), D * Math.Sin(θ)) ||> newVector2D
+
+let ortogonalVector2D (v1 : vector2D<'u>) =
+  (-v1.y, v1.x) ||> newVector2D
 
 let newVector3D (x1 : float<'u>) (y1 : float<'u>) (z1 : float<'u>) =
   let object : vector3D<'u> = {x = x1; y = y1; z = z1}
