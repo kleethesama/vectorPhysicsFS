5e98db5e387026085b1dd2eb4eca6210d050c3d9
kleethesama <38240805+kleethesama@users.noreply.github.com>
2022-08-01 19:29:14 +0200

    Minor changes & test module
    Fixed some typos, small refactor and wrote a test module that can be used to create random vector lists for testing and whatever else it could possibly be used for. It wasn't written for anything particular use, but rather just as practice in my project.

--- Projectile motion.fs
+++ Projectile motion.fs
@@ -5,17 +5,17 @@ open FSharp.Data.UnitSystems.SI.UnitSymbols
 open vectors
 
 let flightTime (vec1 : vector2D<'u>) (a : vector2D<'a>) (θ : float) =
-  let v = vec1 |> magnitudeOfVector2D
-  2.0 * v * Math.Sin(θ) / a.y
+  let D = vec1 |> magnitudeOfVector2D
+  2.0 * D * Math.Sin(θ) / a.y
 
 let maxDistance (vec1 : vector2D<'u>) (a : vector2D<'a>) (θ : float) =
   let D = vec1 |> magnitudeOfVector2D
   2.0 * D * D * Math.Sin(θ) * Math.Cos(θ) / a.y
 
 let getPositionX (vec1 : vector2D<'u>) (θ : float) (t : float<'a>) =
-  let v = vec1 |> magnitudeOfVector2D
-  v * Math.Cos(θ) * t
+  let D = vec1 |> magnitudeOfVector2D
+  D * Math.Cos(θ) * t
 
-let getPositionY (vec1 : vector2D<'u>) (a : vector2D<'a>) (θ : float) (t : float<'a>) =
-  let v = vec1 |> magnitudeOfVector2D
-  (v * Math.Sin(θ) - a.y * t) * t
+let getPositionY (vec1 : vector2D<'u>) (a : vector2D<'a>) (θ : float) (t : float<'q>) =
+  let D = vec1 |> magnitudeOfVector2D
+  (D * Math.Sin(θ) - a.y * t) * t
