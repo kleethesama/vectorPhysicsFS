7ea920046f421b652754d789aa6cfb781594860a
kleethesama <38240805+kleethesama@users.noreply.github.com>
2022-08-05 18:48:35 +0200

    Early implementation and test of GUI
    Has a working window where the user is prompted for a vector. The user can insert input why typing into the textbox below the prompt, and the user's input will the shown below the textbox as it is being typed. This is just testing, but in the future this will be used to have the app draw vectors as the user type in their input.

---
+++ GUI.fs
@@ -0,0 +1,46 @@
+﻿namespace VetorPhysicsApp
+
+module GUI =
+    open Avalonia.Controls
+    open Avalonia.FuncUI
+    open Avalonia.FuncUI.DSL
+    open Avalonia.Layout
+
+    let view =
+        Component(fun ctx ->
+            let state = ctx.useState ""
+            DockPanel.create [
+                DockPanel.verticalAlignment VerticalAlignment.Center
+                DockPanel.horizontalAlignment HorizontalAlignment.Center
+                DockPanel.children [
+                    TextBlock.create [
+                        TextBlock.dock Dock.Top
+                        TextBlock.fontSize 48.0
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text "Enter a vector in the format: (x,y)"
+                    ]
+                    TextBlock.create [
+                        TextBlock.dock Dock.Bottom
+                        TextBlock.fontSize 28.0
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (string state.Current)
+                    ](*
+                    Line.create [
+                      Shapes.Line.startPoint (50.0, 25.0)
+                      Shapes.Line.endPoint (100.0, 300.0)
+                    ]
+                    Canvas.create [
+                      Canvas.left 25.0
+                      Canvas.top 25.0
+                      Canvas.right 25.0
+                      Canvas.bottom 25.0
+                    ]*)
+                    TextBox.create [
+                      TextBox.text ""
+                      TextBox.fontSize 28.0
+                      TextBox.horizontalAlignment HorizontalAlignment.Center
+                      TextBox.onTextChanged (fun newString -> newString |> state.Set)
+                    ]
+                ]
+            ]
+        )
---
+++ Program.fs
@@ -0,0 +1,40 @@
+﻿namespace VetorPhysicsApp
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Input
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "Vector Physics"
+        base.Width <- 3.75 * 400.0
+        base.Height <- 2.25 * 400.0
+        this.Content <- GUI.view
+
+        this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme(baseUri = null, Mode = FluentThemeMode.Dark))
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
